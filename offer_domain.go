0b4fa0bddffcf700fa533e3bccc20e227c4ad85a
David trujillo <davidtrujillo2434@gmail.com>
2025-04-18 22:49:14 -0500

    refactor: Add Offer struct and methods for updating details and state

---
+++ offer_domain.go
@@ -0,0 +1,46 @@
+package shareddomain
+
+import (
+	"errors"
+	"time"
+)
+
+type Offer struct {
+	OfferId     string
+	CreatedAt   time.Time
+	Description string
+	ExpireAt    time.Time
+	Name        string
+	PosId       string
+	State       string
+	Type        string
+}
+
+var (
+	StateInactive = "Created"
+	StateActive   = "Offered"
+)
+
+func (o *Offer) Update(name, description string, expireAt time.Time) error {
+	if name == "" {
+		return errors.New("El nombre no puede estar vacío")
+	}
+	if description == "" {
+		return errors.New("La descripcion no puede estar vacía")
+	}
+	if expireAt.IsZero() {
+		return errors.New("La fecha de expiración no puede estar vacía")
+	}
+	o.Name = name
+	o.Description = description
+	o.ExpireAt = expireAt
+	return nil
+}
+
+func (o *Offer) UpdateState(state string) error {
+	if state == "" {
+		return errors.New("El estado no puede estar vacío")
+	}
+	o.State = state
+	return nil
+}
